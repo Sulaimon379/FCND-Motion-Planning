549c3f31b08d845f3ecce0dc50db116eda50b5e6
eselwhy <sly33779@yahoo.com>
2018-06-30 21:43:27 -0700

    completed motion_planning.py

--- motion_planning.py
+++ motion_planning.py
@@ -1,6 +1,7 @@
 import argparse
 import time
 import msgpack
+import numpy as np
 from enum import Enum, auto
 
 
@@ -10,16 +11,46 @@ from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
 from udacidrone.frame_utils import global_to_local
 
-import pandas as pd
+
+#prune_the path
+def point(p):
+    return np.array([p[0], p[1], 1.])
+
+def collinearity_check(p1, p2, p3, epsilon=1e-6): 
+    collinear = False
+    #Create the matrix out of three points
+    mat = np.vstack((point(p1), point(p2), point(p3)))    
+    det = np.linalg.det(mat)
+    # Set collinear to True if the determinant is less than epsilon
+    if det < epsilon:
+        collinear = True
+        
+    return collinear
+
+def prune_path(path):
+    pruned_path = [p for p in path]
+
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = pruned_path[i]
+        p2 = pruned_path[i+1]
+        p3 = pruned_path[i+2]
+
+        if collinearity_check(p1, p2, p3):
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+    return pruned_path
+
 
 class States(Enum):
-    MANUAL = auto()
-    ARMING = auto()
-    TAKEOFF = auto()
-    WAYPOINT = auto()
-    LANDING = auto()
-    DISARMING = auto()
-    PLANNING = auto()
+	MANUAL = auto()
+	ARMING = auto()
+	TAKEOFF = auto()
+	WAYPOINT = auto()
+	LANDING = auto()
+	DISARMING = auto()
+	PLANNING = auto()
 
 
 class MotionPlanning(Drone):
@@ -111,7 +142,7 @@ class MotionPlanning(Drone):
         data = msgpack.dumps(self.waypoints)
         self.connection._master.write(data)
 	
-	def plan_path(self):
+    def plan_path(self):
         self.flight_state = States.PLANNING
         print("Searching for a path ...")
         TARGET_ALTITUDE = 5
@@ -120,19 +151,19 @@ class MotionPlanning(Drone):
         self.target_position[2] = TARGET_ALTITUDE
 
         # TODO: read lat0, lon0 from colliders into floating point values
-		colliders_file = 'colliders.csv'		
-		lat_lon = np.loadtxt(colliders_file, delimiter=',', dtype='str', usecols=[0,1])
-		lat0 = float((lat_lon[0][0])[5:])
-		lon0 = float((lat_lon[0][1])[5:])
+        colliders_file = 'colliders.csv'		
+        lat_lon = np.loadtxt(colliders_file, delimiter=',', dtype='str', usecols=[0,1])
+        lat0 = float((lat_lon[0][0])[5:])
+        lon0 = float((lat_lon[0][1])[5:])
         
         # TODO: set home position to (lon0, lat0, 0)
-		self.set_home_position(lon0, lat0, 0)
+        self.set_home_position(lon0, lat0, 0)
 
         # TODO: retrieve current global position
-		 global_position = np.array([self._longitude, self._latitude,  self._altitude])
+        global_position = np.array([self._longitude, self._latitude,  self._altitude])
 		 
         # TODO: convert to current local position using global_to_local()
-		self._north, self._east, self._down = global_to_local(global_position, self.global_home)
+        self._north, self._east, self._down = global_to_local(global_position, self.global_home)        
         
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position, self.local_position))
         # Read in obstacle map
@@ -142,21 +173,25 @@ class MotionPlanning(Drone):
         grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
-        grid_start = (-north_offset, -east_offset)
+        #grid_start = (-north_offset, -east_offset)
         # TODO: convert start position to current position rather than map center
-        grid_start = (int(local_position[0])-north_offset, int(local_position[1])-east_offset)
+        grid_start = (int(self._north)-north_offset, int(self._east)-east_offset)
 		 
         # Set goal as some arbitrary position on the grid
         grid_goal = (-north_offset + 10, -east_offset + 10)
-        # TODO: adapt to set goal as latitude / longitude position and convert
+        # TODO: adapt to set goal as latitude / longitude position and convert        
+        goal_pos = global_to_local([-122.396064, 37.793991, self._altitude], self.global_home)#Corner of Drumm and California
+        grid_goal = (int(goal_pos[0]-north_offset), int(goal_pos[1]-east_offset))
 
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
         print('Local Start and Goal: ', grid_start, grid_goal)
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
+        #path, cost = a_star(grid, heuristic, grid_start, grid_goal)
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
+        path = prune_path(path)
 
         # Convert path to waypoints
         waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
@@ -179,13 +214,13 @@ class MotionPlanning(Drone):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default=5760, help='Port number')
-    parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--port', type=int, default=5760, help='Port number')
+	parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+	args = parser.parse_args()
 
-    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60)
-    drone = MotionPlanning(conn)
-    time.sleep(1)
+	conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60)
+	drone = MotionPlanning(conn)
+	time.sleep(1)
 
-    drone.start()
+	drone.start()
--- planning_utils.py
+++ planning_utils.py
@@ -1,15 +1,8 @@
 from enum import Enum
 from queue import PriorityQueue
 import numpy as np
-import utm
 
 
- def global_to_local(global_position, global_home):    
-	(east_home, north_home, _, _) = utm.from_latlon(global_home[1], global_home[0])		
-	(east, north, _, _) = utm.from_latlon(global_position[1], global_position[0])
-	local_position = numpy.array([north - north_home, east - east_home, -(global_position[2] - global_home[2])])
-		
-	return local_position
 def create_grid(data, drone_altitude, safety_distance):
     """
     Returns a grid representation of a 2D configuration space
@@ -63,6 +56,12 @@ class Action(Enum):
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
 
+    #TODO : add diagonal motion
+    NE = (-1, 1,np.sqrt(2))
+    SE = ( 1, 1,np.sqrt(2))
+    SW = ( 1,-1,np.sqrt(2))
+    NW = (-1,-1,np.sqrt(2))
+
     @property
     def cost(self):
         return self.value[2]
@@ -92,6 +91,16 @@ def valid_actions(grid, current_node):
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
 
+    #TODO diagonal actions
+    if x - 1 < 0 or y - 1 < 0 or grid[x-1,y-1] == 1:
+        valid_actions.remove(Action.NW)
+    if x - 1 < 0 or y + 1 > m or grid[x-1,y+1] == 1:
+        valid_actions.remove(Action.NE)
+    if x + 1 > n or y - 1 < 0 or grid[x+1,y-1] == 1:
+        valid_actions.remove(Action.SW)
+    if x + 1 > n or y + 1 > m or grid[x+1,y+1] == 1:
+        valid_actions.remove(Action.SE)
+
     return valid_actions
 
 
@@ -105,16 +114,16 @@ def a_star(grid, h, start, goal):
 
     branch = {}
     found = False
-    
+
     while not queue.empty():
         item = queue.get()
         current_node = item[1]
         if current_node == start:
             current_cost = 0.0
-        else:              
+        else:
             current_cost = branch[current_node][0]
-            
-        if current_node == goal:        
+
+        if current_node == goal:
             print('Found a path.')
             found = True
             break
@@ -125,12 +134,12 @@ def a_star(grid, h, start, goal):
                 next_node = (current_node[0] + da[0], current_node[1] + da[1])
                 branch_cost = current_cost + action.cost
                 queue_cost = branch_cost + h(next_node, goal)
-                
-                if next_node not in visited:                
-                    visited.add(next_node)               
+
+                if next_node not in visited:
+                    visited.add(next_node)
                     branch[next_node] = (branch_cost, current_node, action)
                     queue.put((queue_cost, next_node))
-             
+
     if found:
         # retrace steps
         n = goal
@@ -143,11 +152,10 @@ def a_star(grid, h, start, goal):
     else:
         print('**********************')
         print('Failed to find a path!')
-        print('**********************') 
+        print('**********************')
     return path[::-1], path_cost
 
 
 
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
-
